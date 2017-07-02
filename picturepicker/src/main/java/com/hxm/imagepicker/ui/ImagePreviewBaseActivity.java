fab69e940f6f9d24718070a97ff956fd054e1e1d
hexiaomeng <1247535723@qq.com>
2017-07-02 12:51:43 +0800

    修复预览图片数量错误问题

--- picturepicker/src/main/java/com/hxm/imagepicker/ui/ImagePreviewBaseActivity.java
+++ picturepicker/src/main/java/com/hxm/imagepicker/ui/ImagePreviewBaseActivity.java
@@ -44,7 +44,7 @@ public abstract class ImagePreviewBaseActivity extends ImageBaseActivity {
         isFromItems = getIntent().getBooleanExtra(ImagePicker.EXTRA_FROM_ITEMS, false);
         //判断是不是点击预览按钮进入此界面的
         if (isFromItems) {
-            mImageItems = imagePicker.getSelectedImages();
+            mImageItems = getIntent().getParcelableArrayListExtra(ImagePicker.EXTRA_FROM_ITEMS);
         } else {
             mImageItems = imagePicker.getCurrentImageFolderItems();
         }
